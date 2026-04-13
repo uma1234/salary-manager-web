c5b37a51afe2d8691b8d2d612144848d27e9d81c
uma1234 <umarkal3@gmail.com>
2026-04-13 16:28:36 +0530

    feat: add employee form with service integration

---
+++ src/app/components/employee-form/employee-form.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-employee-form',
+  templateUrl: './employee-form.component.html',
+  styleUrl: './employee-form.component.css'
+})
+export class EmployeeFormComponent {
+
+employee: any = {
+  first_name: '',
+  last_name: '',
+  job_title: '',
+  country: '',
+  salary: 0,
+  department: ''
+};
+  // employee: any = {};
+
+ constructor(private api: ApiService) {}
+
+ ngOnInit() {
+  this.api.selectedEmployee.subscribe((emp) => {
+    if (emp) {
+      this.employee = { ...emp };
+    }
+  });
+}
+  submit() {
+    this.api.addEmployee(this.employee).subscribe({
+      next: () => {
+        alert('Employee Added');
+        this.employee = {
+          first_name: '',
+          last_name: '',
+          job_title: '',
+          country: '',
+          salary: 0,
+          department: ''
+        };
+      },
+      error: (err) => {
+        console.log('❌ FULL ERROR:', err);
+        console.log('❌ ERROR STATUS:', err.status);
+        console.log('❌ ERROR MESSAGE:', err.message);
+        console.log('❌ ERROR BODY:', err.error);
+
+        alert('Failed to add employee');
+      }
+    });
+  }
+
+   edit(emp: any) {
+    this.employee = { ...emp }; // fill form
+  }
+
+  update() {
+    this.api.updateEmployee(this.employee.id, this.employee)
+      .subscribe(() => {
+        alert('Employee Updated');
+        this.reset();
+      });
+  }
+
+  reset() {
+    this.employee = {};
+  }
+}
+
+
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private baseUrl = 'http://localhost:3000';
+  selectedEmployee = new BehaviorSubject<any>(null);
+  constructor(private http: HttpClient) {}
+
+
+  getEmployees(page: number) {
+    return this.http.get(`${this.baseUrl}/employees?page=${page}`);
+  }
+
+  addEmployee(data: any) {
+    return this.http.post(`${this.baseUrl}/employees`, {
+      employee: data
+    });
+  }
+
+  updateEmployee(id: number, data: any) {
+  return this.http.put(`${this.baseUrl}/employees/${id}`, {
+    employee: data
+  });
+  }
+
+  deleteEmployee(id: number) {
+    return this.http.delete(`${this.baseUrl}/employees/${id}`);
+  }
+
+}
